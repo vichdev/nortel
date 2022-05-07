f8c58afafbb664bdddb09f1c8d2c52db1d3b9764
VictorHugo <victor.hugo.antunes.n@gmail.com>
2022-05-07 18:15:33 -0300

    1.0v - Configuração inicial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <div>
+        <h1>hello World!</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// @ts-ignore
+import App from "./App.tsx";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
